80075c693404384d8255386dcfaaaa51e24e0ec4
Antonela Chiba <antonela@creditstacks.com>
2020-04-13 14:10:39 +0300

    added index.js file

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = require('./db/utils');
+module.exports = require('./db/index');
+module.exports = require('./db/parser');
+module.exports = require('./db/model');
+module.exports = require('./email/gmail/index');
+module.exports = require('./email/gmail/credentials');
+module.exports = require('./email/gmail/token');
+module.exports = require('./store/index');
+module.exports = require('./store/schema');
+module.exports = require('./utils/functions');
+module.exports = require('./web/action');
+module.exports = require('./web/functions');
+module.exports = require('./web/wait');
+module.exports = require('./web/page/index');
+
+
+
